8560bf74489999d06e4f207e3db5a28f3a61fb1b
skb <136301025+Kanhu5290@users.noreply.github.com>
2025-10-12 18:40:39 +0530

    Create Learningpythonhardway_ex10.py

---
+++ Lphy_ex10.py
@@ -0,0 +1,23 @@
+# Learn python the hard way
+# Exercise 10 
+# Excape sequence in  string that tell which double column is string and which is represent string
+"I am 6'2\"tall."
+# escape double quote inside string
+'I am 6\'2\"tall.'
+# escape single quote inside string
+
+tabby_cat = "\tI'm tabbed in." # \t used for tab before string
+# \is used to create new lin
+persian_cat = "I'm split\non a line."
+# \ used with \to print escape sequence
+backslash_cat = "I'm\\a\\cat."
+fat_cat="""
+I'll do a list:
+\t*cat food
+\t*Fishies
+\t*Catnip\n\t*Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
